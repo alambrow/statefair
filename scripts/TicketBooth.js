2e04253741ac86a0e975897bca9e99b8cc8a913a
alambrow <alexander.lambrow@gmail.com>
2021-05-04 09:29:58 -0500

    riders are popping up blue

--- scripts/TicketBooth.js
+++ scripts/TicketBooth.js
@@ -1,12 +1,21 @@
 const contentTarget = document.querySelector(".entry")
 const eventHub = document.querySelector("#state-fair")
 
-eventHub.addEventListener()
+eventHub.addEventListener(
+    "click",
+    (event) => {
+        // To check whether the user clicked RideTicket button
+        if (event.target.id === "rideTicket") {
+            const rideEvent = new CustomEvent("rideTicketPurchased")
+            eventHub.dispatchEvent(rideEvent)
+        }
+    }
+)
 
 export const TicketBooth = () => {
     contentTarget.innerHTML = `
         <div class="ticketBooth">
-            Add button here
+            <button id="rideTicket">Ride Ticket</button>
         </div>
     `
 }
