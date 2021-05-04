d9f3ce8d9a20a6746c9045495985c51b1f9fee84
alambrow <alexander.lambrow@gmail.com>
2021-05-04 14:30:24 -0500

    project complete, with funky styling

--- scripts/main.js
+++ scripts/main.js
@@ -14,3 +14,58 @@ FullFoodTicketHolders()
 FullGameTicketHolders()
 FullRideTicketHolders()
 FullSideshowTicketHolders()
+
+const eventHub = document.querySelector("#state-fair")
+const TicketCounter = document.querySelector(".ticketDisplay")
+
+
+// renders html for ticket count display
+const ticketDisplay = () => {
+    TicketCounter.innerHTML = "Total tickets purchased: " + ticketCount
+    return TicketCounter.innerHTML
+}
+
+// bean counter + event listeners to register purchase (click) events
+let ticketCount = 0
+
+eventHub.addEventListener(
+    "rideTicketPurchased",
+    (CustomEvent) => {
+        ticketCount += 1
+        ticketDisplay()
+    }
+    )    
+
+eventHub.addEventListener(
+    "foodTicketPurchased",
+    (CustomEvent) => {
+        ticketCount += 1
+        ticketDisplay()
+    }
+)
+
+eventHub.addEventListener(
+    "gameTicketPurchased",
+    (CustomEvent) => {
+        ticketCount += 1
+        ticketDisplay()
+    }
+)
+
+eventHub.addEventListener(
+    "sideshowTicketPurchased",
+    (CustomEvent) => {
+        ticketCount += 1
+        ticketDisplay()
+    }
+)
+
+eventHub.addEventListener(
+    "fullPackageTicketPurchased",
+    (CustomEvent) => {
+        ticketCount += 1
+        ticketDisplay()
+    }
+)
+
+ticketDisplay()
